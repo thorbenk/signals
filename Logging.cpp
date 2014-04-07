6eff0c8a93eff810514cf4d7722c07698513d90c
Jan Funke <funke@ini.ch>
2014-04-07 11:26:50 +0200

    made logging quiet by default

--- Logging.cpp
+++ Logging.cpp
@@ -2,7 +2,9 @@
 
 namespace signals {
 
-	logger::LogChannel signalslog("signalslog", "[signals] ");
+	// let the signals log be quiet by default -- if you really want to read it, 
+	// enable it explicitly via the command line
+	logger::LogChannel signalslog("signalslog", "[signals] ", logger::Quiet);
 
 } // namespace signals
 
